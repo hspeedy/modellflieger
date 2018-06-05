8bc44c9c965a19c1f2c3154d767d03025cc601c1
hspeedy <hspeedy@users.noreply.github.com>
2018-06-05 20:33:05 +0200

    working side menu

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { TemplatesModule } from './templates/templates.module';
@@ -13,6 +14,7 @@ import { ServicesModule } from './services/services.module';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NgbModule.forRoot(),
     AtomsModule,
     MoleculesModule,
--- web/src/app/templates/app-frame-template/app-frame-template.component.ts
+++ web/src/app/templates/app-frame-template/app-frame-template.component.ts
@@ -1,26 +1,46 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { trigger, state, style, transition, animate} from '@angular/animations';
 import { AppFrameService } from '../../services/application/app-frame.service';
 
 @Component({
   selector: 'app-frame-template',
   templateUrl: './app-frame-template.component.html',
-  styleUrls: ['./app-frame-template.component.css']
+  styleUrls: ['./app-frame-template.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(-100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
+
 })
 export class AppFrameTemplateComponent implements OnInit, AfterViewInit {
+
+  menuState = 'out';
 
   constructor(private _appFrameService: AppFrameService) {
 
   }
 
   ngOnInit() {
-    
+
   }
 
   ngAfterViewInit() {
-    this._appFrameService.toggleCallback = this.toggle;
+    this._appFrameService.toggleCallback = this.toggle.bind(this);
   }
 
   toggle(toggle: boolean) {
-    const i = 0;
+    if (!toggle) {
+      this.menuState = 'out';
+    } else {
+      this.menuState = 'in';
+    }
   }
 }
