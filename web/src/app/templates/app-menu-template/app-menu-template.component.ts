81ff956d572436f532f1a64283ae943bcaa20aec
hspeedy <hspeedy@users.noreply.github.com>
2018-06-24 09:13:28 +0200

    resizing

--- web/src/app/templates/app-menu-template/app-menu-template.component.ts
+++ web/src/app/templates/app-menu-template/app-menu-template.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu-template',
   templateUrl: './app-menu-template.component.html',
-  styleUrls: ['./app-menu-template.component.css']
+  styleUrls: ['./app-menu-template.component.scss']
 })
 export class AppMenuTemplateComponent implements OnInit {
 
--- web/src/app/templates/page-template/page-template.component.ts
+++ web/src/app/templates/page-template/page-template.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-template',
   templateUrl: './page-template.component.html',
-  styleUrls: ['./page-template.component.css']
+  styleUrls: ['./page-template.component.scss']
 })
 export class PageTemplateComponent implements OnInit {
 
